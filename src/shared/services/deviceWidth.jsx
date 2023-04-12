342c2e90faa7a6c00395ae187a7704da64dd9779
veras92 <110496079+veras92@users.noreply.github.com>
2023-04-12 15:16:11 +0300

    routes+deviceWidth

---
+++ src/shared/services/deviceWidth.jsx
@@ -0,0 +1,12 @@
+
+export const deviceSize = {
+  mobile: 375,
+  tablet: 768,
+  desktop: 1440,
+}
+
+export const deviceSizePx = {
+  mobile: "375px",
+  tablet: "768px",
+  desktop: "1440px",
+}
---
+++ src/shared/services/routes.jsx
@@ -0,0 +1,11 @@
+export const routes = {
+  register: '/register',
+  login: '/login',
+  calendar: '/calendar',
+  calendarMonth: '/calendar/month',
+  calendarDay: '/calendar/day',
+  currentDate: '/calendar/month/currentDate',
+  currentDay: '/calendar/day/currentDay',
+  account: '/account'
+
+}
