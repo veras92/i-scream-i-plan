6f9aa7bff396b76469c7e883f645267db2d913a9
veras92 <110496079+veras92@users.noreply.github.com>
2023-04-12 10:50:42 +0000

    Deploying to gh-pages from @ veras92/i-scream-i-plan@9999c322746687fc817ba99a46d7c00b5e5eb20f 🚀

--- static/js/852.1c8f2d5b.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[852],{852:function(e,r,n){n.r(r),n.d(r,{default:function(){return c}});var t=n(87),i=n(184);function c(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)("div",{children:"MainPage"}),(0,i.jsx)(t.rU,{to:"/login",children:"Sign up"}),(0,i.jsx)(t.rU,{to:"/register",children:"Log in "})]})}}}]);
-//# sourceMappingURL=852.1c8f2d5b.chunk.js.map
