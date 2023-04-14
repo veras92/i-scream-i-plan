7a10b0ffe53b13a0363fa4bf44fd666d3e5b48ea
AnttonnIT <anttonnit@gmail.com>
2023-04-14 18:00:36 +0300

    Routes
    накидав приватні\публічні роути, але не підключав їх

---
+++ src/shared/services/RestrictedRoute.js
@@ -0,0 +1,7 @@
+import { useAuth } from 'hooks/useAuth';
+import { Navigate } from 'react-router-dom';
+
+export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
