ec54ce5315b6b22dd20a91145e4669555ccdaa7f
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2023-04-16 23:35:41 +0300

    main page styles added partially
    Also updated Modal component colors to match theme

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,20 +1,27 @@
-import { Link } from 'react-router-dom';
-import React from "react";
+import React from 'react';
 
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { reviews, Rating } from './reviews';
 
 import sprite from 'shared/icons/sprite.svg';
-import { 
-  Svg, 
-  ArrowLeft, 
-  ArrowRight, 
-  SliderContainer, 
-  Avatar, 
+
+import {
+  Svg,
+  ArrowLeft,
+  ArrowRight,
+  SliderContainer,
+  Avatar,
   Comment,
-  Name, 
+  Name,
+  StyledHeader,
+  LogoImg,
+  AppTitle,
+  ButtonWrapper,
+  SignUpBtn,
+  ButtonSvg,
+  LogInBtn,
 } from './MainPage.styled';
 
 import one from './images/one.png';
@@ -25,11 +32,10 @@ import goose from 'shared/icons/goose.svg';
 function SamplePrevArrow(props) {
   const { onClick } = props;
   return (
-    <ArrowRight
-      onClick={onClick}>
-        <Svg width="61" height="61">
-            <use href={`${sprite}#icon-arrow-right`} />
-        </Svg>
+    <ArrowRight onClick={onClick}>
+      <Svg width="61" height="61">
+        <use href={`${sprite}#icon-arrow-right`} />
+      </Svg>
     </ArrowRight>
   );
 }
@@ -37,12 +43,11 @@ function SamplePrevArrow(props) {
 function SampleNextArrow(props) {
   const { onClick } = props;
   return (
-    <ArrowLeft
-    onClick={onClick}>
+    <ArrowLeft onClick={onClick}>
       <Svg width="61" height="61">
         <use href={`${sprite}#icon-arrow-left`} />
       </Svg>
-      </ArrowLeft>
+    </ArrowLeft>
   );
 }
 
@@ -53,81 +58,97 @@ const settings = {
   slidesToShow: 2,
   slidesToScroll: 1,
   nextArrow: <SampleNextArrow />,
-  prevArrow: <SamplePrevArrow />
-
+  prevArrow: <SamplePrevArrow />,
 };
 
 export default function MainPage() {
   return (
     <>
-      <div style={{ margin: '50px' }}>
-      <header>   
-        <img width="150" src={goose} alt="goose"/>       
-        <h1>Goose Track</h1>
-        <Link to="/register">Sign up</Link>
-        <Link to="/login">Log in </Link>
-    </header>
-    <main>
-      <section>
-      <ul>
-        <li>
-          <div>
-          <h2>1.</h2>
-            <h3>CALENDAR</h3>
-            <h3>VIEW</h3>
-            <span>GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.</span>
-        </div>
-        <div>
-        <img src={one} alt="calendar" />
-        </div>
-      </li>
-        <li>
-          <div>
-          <h2>2.</h2>
-            <h3>SIDEBAR</h3>
-            <span>GooseTrack offers easy access to your account settings, calendar, and filters. The "My Account" section allows you to manage your profile information and preferences, while the calendar provides a quick and convenient way to view your upcoming events and tasks.</span>
-        </div>
-        <div>
-          <img src={two} alt="sidebar" />
-        </div>
-      </li>
-        <li>
-          <div>
-          <h2>3.</h2>
-            <h3>ALL IN</h3>
-            <h3>ONE</h3>
-            <span>GooseTrack is an all-in-one productivity tool that helps you stay on top of your tasks, events, and deadlines. Say goodbye to scattered to-do lists and hello to streamlined productivity with GooseTrack.</span>
-        </div>
-        <div>
-        <img src={three} alt="all in" />
-        </div>
-      </li>        
-      </ul>
-      </section>
-    </main>
-      <section>
-        <h2>REVIEWS</h2>  
-        <Slider {...settings}>
-          {reviews.map((review) => (
-            <div key={review.id}>
-              <SliderContainer>                
+      <StyledHeader>
+        <LogoImg src={goose} alt="goose" />
+        <AppTitle>GooseTrack</AppTitle>
+        <ButtonWrapper>
+          <SignUpBtn to="/register">Sign up</SignUpBtn>
+          <LogInBtn to="/login">
+            Log in{' '}
+            <ButtonSvg>
+              <use href={`${sprite}#icon-Log-in`} />
+            </ButtonSvg>
+          </LogInBtn>
+        </ButtonWrapper>
+      </StyledHeader>
+      <main>
+        <section>
+          <ul>
+            <li>
               <div>
-                <Avatar src={review.photo} alt={review.name} />
+                <h2>1.</h2>
+                <h3>CALENDAR</h3>
+                <h3>VIEW</h3>
+                <span>
+                  GooseTrack's Calendar view provides a comprehensive overview
+                  of your schedule, displaying all your tasks, events, and
+                  appointments in a visually appealing and intuitive layout.
+                </span>
               </div>
               <div>
-                <Name>{review.name}</Name>
-                <Rating rating={review.rating} />
-                <Comment>{review.text}</Comment>      
-              </div>    
-              </SliderContainer>      
+                <img src={one} alt="calendar" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2>2.</h2>
+                <h3>SIDEBAR</h3>
+                <span>
+                  GooseTrack offers easy access to your account settings,
+                  calendar, and filters. The "My Account" section allows you to
+                  manage your profile information and preferences, while the
+                  calendar provides a quick and convenient way to view your
+                  upcoming events and tasks.
+                </span>
+              </div>
+              <div>
+                <img src={two} alt="sidebar" />
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2>3.</h2>
+                <h3>ALL IN</h3>
+                <h3>ONE</h3>
+                <span>
+                  GooseTrack is an all-in-one productivity tool that helps you
+                  stay on top of your tasks, events, and deadlines. Say goodbye
+                  to scattered to-do lists and hello to streamlined productivity
+                  with GooseTrack.
+                </span>
+              </div>
+              <div>
+                <img src={three} alt="all in" />
+              </div>
+            </li>
+          </ul>
+        </section>
+      </main>
+      <section>
+        <h2>REVIEWS</h2>
+        <Slider {...settings}>
+          {reviews.map(review => (
+            <div key={review.id}>
+              <SliderContainer>
+                <div>
+                  <Avatar src={review.photo} alt={review.name} />
+                </div>
+                <div>
+                  <Name>{review.name}</Name>
+                  <Rating rating={review.rating} />
+                  <Comment>{review.text}</Comment>
+                </div>
+              </SliderContainer>
             </div>
           ))}
-        </Slider> 
+        </Slider>
       </section>
-      </div>
     </>
   );
 }
-
-
-
