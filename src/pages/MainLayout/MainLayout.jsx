ba9c91bef1d59deb4512a73df196ff0bef916f0f
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2023-04-18 00:09:19 +0300

    sidebar logic + function to get screen size
    Function works but it is called too many times. Dont have a fix yet, but all works

--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -3,7 +3,7 @@
 // 3. Комопонент рендерить:
 //  - Header - модуль що відображається на всіх сторінках авторизованого юзера. Показує загальну інформацію та допоміжний інтерфейс роботи з обліковим записом.
 //  - SideBar - модуль що відображається на всіх сторінках авторизованого юзера, на планшеті та мобілці знаходиться в бургер-меню. Показує навігацію між сторінками та кнопку виходу з облікового запису. "
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 import { Header } from 'modules/Header/Header';
@@ -12,10 +12,40 @@ import { Outlet } from 'react-router-dom';
 import { MainWrapper } from 'shared/styles/components';
 import { Wrapper } from './MainLayout.styled';
 
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
+
 export default function MainLayout() {
   const [isVisible, setVisible] = useState(false);
   const toggleSidebar = () => setVisible(isVisible => !isVisible);
 
+  // const size = useWindowSize();
+  // console.log(size.width);
+
+  // size.width > 1440 ? setVisible(true) : setVisible(false);
   return (
     <>
       <Wrapper>
