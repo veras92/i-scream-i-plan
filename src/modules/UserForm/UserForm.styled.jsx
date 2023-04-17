b95b6e91c474e61167565b911cbaeb4f755c80ec
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-04-17 18:17:30 +0300

    UserFormStyles

--- src/modules/UserForm/UserForm.styled.jsx
+++ src/modules/UserForm/UserForm.styled.jsx
@@ -1,1 +1,58 @@
 import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 16px;
+
+  // ----------------
+  outline: 1px solid #000;
+  margin: 20px;
+  // -----------------
+
+  & > button {
+    padding: 14px 50px;
+    margin-top: 0px;
+    margin-bottom: 40px;
+
+    @media (min-width: 768px) {
+      padding: 15px 84px;
+    }
+
+    @media (min-width: 1087px) {
+      margin-bottom: 60px;
+    }
+  }
+`;
+
+export const FormBody = styled.div`
+  width: 100%;
+  display: grid;
+  padding: 40px 18px;
+  gap: 18px;
+
+  @media (min-width: 768px) {
+    padding: 40px 0px;
+    max-width: 354px;
+  }
+
+  @media (min-width: 1078px) {
+    padding: 40px 0px;
+    max-width: 758px;
+
+    grid-template-columns: 1fr 1fr;
+    column-gap: 50px;
+    row-gap: 24px;
+  }
+
+  @media (min-width: 1440px) {
+    max-width: 1080px;
+  }
+
+  @media (min-width: 1600px) {
+    max-width: 1440px;
+    padding: 50px;
+    grid-template-columns: repeat(auto-fit, minmax(345px, 1fr));
+  }
+`;
--- src/modules/UserForm/components/UserAvatarField/UserAvatarField.styled.jsx
+++ src/modules/UserForm/components/UserAvatarField/UserAvatarField.styled.jsx
@@ -5,24 +5,104 @@ export const Svg = styled.svg`
   stroke: var(--main-blue-color);
 `;
 
+export const UserAvatarWrapper = styled.div`
+  position: relative;
+
+  text-align: center;
+
+  > h3 {
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 18px;
+    color: #343434;
+
+    margin-bottom: 0.25rem;
+
+    @media (min-width: 768px) {
+      font-size: 18px;
+      margin-bottom: 0.5rem;
+    }
+  }
+
+  > p {
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 14px;
+    color: #616161;
+
+    @media (min-width: 768px) {
+      font-size: 14px;
+      line-height: 18px;
+    }
+  }
+`;
+
 export const Label = styled.label`
   position: relative;
-  display: block;
-  width: 124px;
-  height: 124px;
-  margin-bottom: 20px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 72px;
+  height: 72px;
+
+  margin-bottom: 1.125rem;
+  margin-top: -37px;
+
+  border-radius: 50%;
+  border: 2px solid #3E85F3;
+  background-color: #FFFFFF;
+  overflow: hidden;
+
+  & > img {
+    object-fit: contain;
+  }
+
+  @media (min-width: 768px) {
+    margin-top: 40px;
+    width: 124px;
+    height: 124px;
+  }
+
+  @media (min-width: 1087px) {
+    margin-top: 60px;
+  }
+
 `;
 
 export const HiddenInput = styled.input`
   position: absolute;
-  width: 1px;
-  height: 1px;
-  margin: -1px;
+  top: 1.4375rem;
+  right: 0.875rem;
+
+  display: flex;
+  justify-content: center;
+  aling-itms: center;
+  
+  width: 14px;
+  height: 14px;
+  // margin: -1px;
   border: 0;
   padding: 0;
 
-  white-space: nowrap;
-  clip-path: inset(100%);
-  clip: rect(0 0 0 0);
-  overflow: hidden;
+  border-radius: 50%;
+  background-color: #000;
+
+  @media (min-width: 768px) {
+    width: 24px;
+    height: 24px;
+
+    top: 9.0625rem;
+    right: 1.5rem;
+  }
+
+  @media (min-width: 1087px) {
+    top: 10.3125rem;
+  }
+
+  // white-space: nowrap;
+  // clip-path: inset(100%);
+  // clip: rect(0 0 0 0);
+  // overflow: hidden;
 `;
