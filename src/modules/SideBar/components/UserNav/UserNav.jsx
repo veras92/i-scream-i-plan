49691109653a9605c7dda14ecdf08d717f14e961
AnttonnIT <anttonnit@gmail.com>
2023-04-19 02:10:53 +0300

    SideBar-close

--- src/modules/SideBar/components/UserNav/UserNav.jsx
+++ src/modules/SideBar/components/UserNav/UserNav.jsx
@@ -3,18 +3,36 @@
 //  - /calendar - редіректить на CalendarPage з модулем календаря місяця."
 import sprite from 'shared/icons/sprite.svg';
 import { NavList, StyledLink, NavIcon } from './UserNav.styled';
+import { useWindowSize } from 'pages/MainLayout/MainLayout';
+import { account, calendar } from 'shared/services/routes';
 
-export const UserNav = () => {
+export const UserNav = ({ closeSideBar }) => {
+  const windowSize = useWindowSize();
+  function closeSideBarOnMobile() {
+    if (windowSize.width < 1440) {
+      closeSideBar();
+    }
+  }
   return (
     <>
       <NavList>
-        <StyledLink to="/account">
+        <StyledLink
+          to={account}
+          onClick={() => {
+            closeSideBarOnMobile();
+          }}
+        >
           <NavIcon>
             <use href={`${sprite}#icon-user-check-01`} />
           </NavIcon>
           <span>My account</span>
         </StyledLink>
-        <StyledLink to="/calendar">
+        <StyledLink
+          to={calendar}
+          onClick={() => {
+            closeSideBarOnMobile();
+          }}
+        >
           <NavIcon>
             <use href={`${sprite}#icon-calendar-check-02`} />
           </NavIcon>
