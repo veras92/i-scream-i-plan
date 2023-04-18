05d74128a758931f1d0940076f5c3687fa6e9576
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-04-18 11:51:16 +0300

    tasks slice matchers + app improve

--- src/redux/tasks/tasksSlice.js
+++ src/redux/tasks/tasksSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { authApi } from 'redux/auth/authApi';
 import { reauthApi } from 'redux/auth/reauthApi';
+import { tasksApi } from './tasksApi';
 
 const tasksSlice = createSlice({
   name: 'tasks',
@@ -11,16 +12,60 @@ const tasksSlice = createSlice({
     },
   },
   extraReducers: builder => {
-    builder.addMatcher(
-      isAnyOf(
-        authApi.endpoints.logoutUser.matchFulfilled,
-        authApi.endpoints.logoutUser.matchRejected,
-        reauthApi.endpoints.refreshTokens.matchRejected
-      ),
-      state => {
-        state.length = 0;
-      }
-    );
+    builder
+      .addMatcher(
+        tasksApi.endpoints.getTasksByMonth.matchFulfilled,
+        (_, { payload }) => {
+          return payload;
+        }
+      )
+      .addMatcher(
+        tasksApi.endpoints.createTask.matchFulfilled,
+        (state, { payload }) => {
+          return state.map(item => {
+            return {
+              ...item,
+              tasks: [...item.tasks, ...payload],
+            };
+          });
+        }
+      )
+      .addMatcher(
+        tasksApi.endpoints.changeTask.matchFulfilled,
+        (state, { payload }) => {
+          return state.map(item => {
+            return {
+              ...item,
+              tasks: item.tasks.map(task => {
+                if (task._id === payload._id) {
+                  return payload;
+                }
+                return task;
+              }),
+            };
+          });
+        }
+      )
+      .addMatcher(
+        tasksApi.endpoints.deleteTask.matchFulfilled,
+        (state, { payload }) => {
+          state.forEach(({ tasks }) => {
+            const index = tasks.findIndex(task => task._id === payload._id);
+            if (index) tasks.splice(index, 1);
+          });
+          return state.filter(({ tasks }) => tasks.length !== 0);
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          authApi.endpoints.logoutUser.matchFulfilled,
+          authApi.endpoints.logoutUser.matchRejected,
+          reauthApi.endpoints.refreshTokens.matchRejected
+        ),
+        state => {
+          state.length = 0;
+        }
+      );
   },
 });
 
