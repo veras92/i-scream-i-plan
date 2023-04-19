3b6e32fdaae9e296966808d44b969aaf9811d02a
Kyrylo <shandaliuk3019@gmail.com>
2023-04-19 13:47:10 +0300

    Fixed card

--- src/modules/ChoosedDay/components/ColumnHeadBar/ColumnHeadBar.jsx
+++ src/modules/ChoosedDay/components/ColumnHeadBar/ColumnHeadBar.jsx
@@ -2,6 +2,12 @@
 // 2. Компонент рендерить блок розмітки
 //  - з заголовком колонки,
 //  - кнопкою для створення нової задачі, що відкриває модалку для створення задачі."
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import parse from 'date-fns/parse';
+import { selectDate } from 'redux/date/selectors';
+import { TaskModal } from 'shared/components/TaskModal/TaskModal';
+import { TASK_MODAL_TYPES } from 'shared/services/taskModalTypes';
 import sprite from 'shared/icons/sprite.svg';
 import {
   ColumnTitleWrapper,
@@ -24,14 +30,28 @@ export const ColumnHeadBar = ({ type }) => {
     }
   };
 
+  const [isOpened, setOpening] = useState(false);
+
+  const date = useSelector(selectDate);
+
+  const handleToggleModal = () => setOpening(!isOpened);
+
   return (
     <ColumnTitleWrapper>
       <ColumnTitle>{normalizeTitle(type)}</ColumnTitle>
-      <AddButton type="button">
+      <AddButton type="button" onClick={handleToggleModal}>
         <StyledAddIcon>
           <use href={`${sprite}#icon-plus-circle`} />
         </StyledAddIcon>
       </AddButton>
+      {isOpened && (
+        <TaskModal
+          date={parse(date, 'yyyy-MM-dd', new Date())}
+          type={TASK_MODAL_TYPES.add}
+          onCloseModal={handleToggleModal}
+          category={type}
+        />
+      )}
     </ColumnTitleWrapper>
   );
 };
