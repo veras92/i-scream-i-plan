466b6756a7f390e7b467df939d4d2557e5f8f9fe
AnttonnIT <anttonnit@gmail.com>
2023-04-20 17:03:00 +0300

    fix

--- src/modules/ChoosedDay/components/TasksColumnsList/TasksColumnsList.styled.jsx
+++ src/modules/ChoosedDay/components/TasksColumnsList/TasksColumnsList.styled.jsx
@@ -5,32 +5,39 @@ import { BREAKPOINTS } from 'shared/services/deviceWidth';
 
 const media = facepaint(BREAKPOINTS.map(bp => `@media (min-width: ${bp}px)`));
 export const Conatainer = styled.div`
-  /* height: 568px; */
-  overflow-y: auto;
+  max-height: 432px;
+  overflow-x: auto;
+
+  @media (min-width: 765px) {
+    max-height: 568px;
+  }
 `;
 export const ColumnsWrapper = styled.ul`
-  display: grid;
+  display: flex;
+
+  gap: 16px;
+  justify-content: space-between;
+
+  /* display: grid;
   grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
 
   ${media({
-  gap: ['22px', '22px', '16px', '27px'],
-})};
+    gap: ['22px', '22px', '16px', '27px'],
+  })}; */
 `;
 
 export const StyledColumn = styled.li`
   padding: 18px;
-
-  // min-width: 344px;
+  flex-grow: 1;
+  min-width: 335px;
   background: ${props => props.theme.bcgTernary};
   border: 1px solid ${props => props.theme.disabledSecondary};
   border-radius: 8px;
 
-  /* max-height: 432px; */
-  /* overflow-y: auto; */
+  min-height: 432px;
 
   @media (min-width: 765px) {
     padding: 20px;
-    // max-width: 344px;
-    /* max-height: 568px; */
+    min-width: 344px;
   }
 `;
