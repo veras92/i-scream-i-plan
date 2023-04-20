186575d512a2f98729c2d696b6d6f5ac2d90fd27
AnttonnIT <anttonnit@gmail.com>
2023-04-21 01:44:53 +0300

    Полагодив відображення гуся на календарі дня

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -7,21 +7,41 @@
 
 import { ThemeToggler } from './components/ThemeToggler/ThemeToggler';
 import { UserInfo } from './components/UserInfo/UserInfo';
-import { Wrapper, Info, SectionTitle, Toggler, GooseTask, MotivationTask } from './Header.styled';
-import { useLocation } from 'react-router-dom';
+import {
+  Wrapper,
+  Info,
+  SectionTitle,
+  Toggler,
+  GooseTask,
+  MotivationTask,
+} from './Header.styled';
+import { useLocation, useParams } from 'react-router-dom';
 import sprite from 'shared/icons/sprite.svg';
 
 import { useSelector } from 'react-redux';
-import { selectTasksForToday } from '../../redux/tasks/tasksSlice';
 import gooseTask from 'shared/icons/goose-task.svg';
+import { selectTasks } from 'redux/tasks/selectors';
 
 export const Header = ({ onToggle }) => {
   const location = useLocation();
   const currentPath = location.pathname;
-  const tasksForToday = useSelector(selectTasksForToday);
 
-  const isCalendarPage = currentPath.startsWith('/calendar/');
-  
+  const { currentDay } = useParams();
+  const isCalendarPage = currentPath.startsWith('/calendar/day');
+
+  const tasks = useSelector(selectTasks);
+
+  const haveTasksToday = () => {
+    const tasksForToday = tasks.filter(task => task.date === currentDay);
+    if (tasksForToday.length > 0) {
+      const tasksInProgress = tasksForToday[0].tasks.find(
+        task => task.category === 'to-do' || task.category === 'in-progress'
+      );
+
+      return tasksInProgress;
+    }
+  };
+
   let title = '';
   if (currentPath.startsWith('/account')) {
     title = 'User Profile';
@@ -33,10 +53,16 @@ export const Header = ({ onToggle }) => {
   return (
     <>
       <Wrapper>
-        {isCalendarPage && tasksForToday.length > 0 && <GooseTask src={gooseTask} alt="goose"/>}
+        {isCalendarPage && haveTasksToday() && (
+          <GooseTask src={gooseTask} alt="goose" />
+        )}
         <div>
-        <SectionTitle>{title}</SectionTitle>
-        {isCalendarPage && tasksForToday.length > 0 && <MotivationTask>Let go of the past and focus on the present!</MotivationTask>}
+          <SectionTitle>{title}</SectionTitle>
+          {isCalendarPage && haveTasksToday() && (
+            <MotivationTask>
+              Let go of the past and focus on the present!
+            </MotivationTask>
+          )}
         </div>
         <Toggler
           onClick={() => {
