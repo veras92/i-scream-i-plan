44e73a1f12e2172104251a88357ee1f3e885bac8
AnttonnIT <anttonnit@gmail.com>
2023-04-20 10:35:34 +0300

    fix style

--- src/modules/CalendarToolbar/components/PeriodTypeSelect/PeriodTypeSelect.styled.jsx
+++ src/modules/CalendarToolbar/components/PeriodTypeSelect/PeriodTypeSelect.styled.jsx
@@ -4,40 +4,40 @@ import { NavLink } from 'react-router-dom';
 export const List = styled.ul`
   display: flex;
   margin-top: 18px;
+
   @media (min-width: 768px) {
     margin-top: 0;
   }
 `;
 export const Item = styled.li`
-  background-color: ${props => props.theme.PeriodTypeSelect};
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
 
-  border-radius: 0px 8px 8px 0px;
+  display: flex;
+`;
+export const StyledNavLink = styled(NavLink)`
+  color: ${props => props.theme.activeText};
+
+  background-color: ${props => props.theme.PeriodTypeSelect};
 
   &.active {
     background-color: ${props => props.theme.PeriodTypeSelectDisabled};
   }
-  &:first-of-type {
+  &.month {
     padding: 8px 16px;
     border-radius: 8px 0 0 8px;
   }
-
-  &:last-child {
+  &.day {
     padding: 8px 25px;
     border-radius: 0 8px 8px 0;
   }
 
   @media (min-width: 768px) {
-    &:last-child {
+    &.day {
       padding: 8px 26px;
     }
   }
-`;
-export const StyledNavLink = styled(NavLink)`
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 18px;
-
-  color: ${props => props.theme.activeText};
 
   @media (min-width: 768px) {
     font-size: 16px;
