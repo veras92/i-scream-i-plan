cdf4e5d74cc9d96f71bfc59f63dfb2ba86e477e6
AnttonnIT <anttonnit@gmail.com>
2023-04-20 16:07:51 +0300

    fix

--- src/pages/MainLayout/MainLayout.styled.jsx
+++ src/pages/MainLayout/MainLayout.styled.jsx
@@ -2,11 +2,11 @@ import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
   display: flex;
-  min-height: 812px;
+  /* min-height: 812px;
   @media (min-width: 768px) {
     min-height: 1024px;
   }
   @media (min-width: 1440px) {
     min-height: 900px;
-  }
+  } */
 `;
