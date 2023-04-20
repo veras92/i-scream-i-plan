8d9fbba75ddd4887305a9905f22017d4c159d2a3
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-04-20 11:34:39 +0300

    позиціонування картинки userInfo

--- src/modules/Header/components/UserInfo/UserInfo.jsx
+++ src/modules/Header/components/UserInfo/UserInfo.jsx
@@ -1,36 +1,45 @@
 // "1. Компонент підписаний на глобальний стейт з якого отримує ім'я користувача та url з фото користувача
 // 2. Компонент відображає ім'я користувача та його зображення/аватар.
 // 3. Якщо зображення відсутнє на його місці повинна відображатись перша буква імені в верхньому регістрі"
-import { Wrapper, UserName, UserPicture, UserNameIcon, BackgroundName } from './UserInfo.styled';
+import {
+  Wrapper,
+  UserName,
+  UserPicture,
+  UserNameIcon,
+  BackgroundName,
+  ImgWrap,
+} from './UserInfo.styled';
 import { useAuth } from 'hooks/useAuth';
 
-
-
 export const UserInfo = () => {
-  function getInitials(name) { 
-    if (name) { const initials = name 
-      .split(' ') 
-      .map(word => word.charAt(0))
-      .join('') 
-      .toUpperCase(); 
-    return initials; 
-  } else { 
-    return name; } 
+  function getInitials(name) {
+    if (name) {
+      const initials = name
+        .split(' ')
+        .map(word => word.charAt(0))
+        .join('')
+        .toUpperCase();
+      return initials;
+    } else {
+      return name;
+    }
   }
-  
-  const {name, userImgUrl} = useAuth();
-  
+
+  const { name, userImgUrl } = useAuth();
+
   const displayName = userImgUrl ? (
-    <img src={userImgUrl} alt="UserPicture" />
+    <ImgWrap>
+      <img src={userImgUrl} alt="UserPicture" />
+    </ImgWrap>
   ) : (
-    <BackgroundName className="initials"><UserNameIcon>{getInitials(name)}</UserNameIcon></BackgroundName>
+    <BackgroundName className="initials">
+      <UserNameIcon>{getInitials(name)}</UserNameIcon>
+    </BackgroundName>
   );
   return (
     <Wrapper>
       <UserName>{name}</UserName>
-      <UserPicture>
-      {displayName}
-      </UserPicture> 
+      <UserPicture>{displayName}</UserPicture>
     </Wrapper>
   );
 };
--- src/modules/Header/components/UserInfo/UserInfo.styled.jsx
+++ src/modules/Header/components/UserInfo/UserInfo.styled.jsx
@@ -13,22 +13,31 @@ export const UserName = styled.p`
   color: ${props => props.theme.text};
 `;
 
+export const ImgWrap = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const BackgroundName = styled.div`
   width: 100%;
   height: 100%;
-  background: linear-gradient(0deg, rgba(228,216,30,0.7147233893557423) 9%, rgba(241,255,13,0.8155637254901961) 100%);
+  background: linear-gradient(
+    0deg,
+    rgba(228, 216, 30, 0.7147233893557423) 9%,
+    rgba(241, 255, 13, 0.8155637254901961) 100%
+  );
   display: flex;
   align-items: center;
   justify-content: center;
-
 `;
 
 export const UserNameIcon = styled.p`
-  
   font-size: 18px;
   font-weight: 700;
   line-height: 1.3;
- 
+
   color: black;
 `;
 
@@ -39,8 +48,6 @@ export const UserPicture = styled.div`
   border: 1.8px solid ${props => props.theme.componentAccent};
   border-radius: 50%;
 
-  
-  
   @media (min-width: 765px) {
     width: 44px;
     height: 44px;
