33ac92f346681536bb05b8edc89422371ea213f7
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-04-21 13:48:38 +0300

    вилікувала бекграунд)

--- src/modules/ChoosedDay/ChoosedDay.jsx
+++ src/modules/ChoosedDay/ChoosedDay.jsx
@@ -1,11 +1,12 @@
+import { Wrap } from './ChoosedDay.styled';
 import { DayCalendarHead } from './components/DayCalendarHead/DayCalendarHead';
 import { TasksColumnsList } from './components/TasksColumnsList/TasksColumnsList';
 
 export const ChoosedDay = () => {
   return (
-    <>
+    <Wrap>
       <DayCalendarHead />
       <TasksColumnsList categories={['to-do', 'in-progress', 'done']} />
-    </>
+    </Wrap>
   );
 };
