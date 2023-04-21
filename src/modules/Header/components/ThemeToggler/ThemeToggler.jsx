04f35f95d272eba8ae43bc648b4f82af9dfe8083
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-04-21 10:01:50 +0300

    theme

--- src/modules/Header/components/ThemeToggler/ThemeToggler.jsx
+++ src/modules/Header/components/ThemeToggler/ThemeToggler.jsx
@@ -1,16 +1,21 @@
 import sprite from 'shared/icons/sprite.svg';
-import { toggleTheme } from 'redux/theme/operations';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectTheme } from 'redux/theme/selectors';
 import { THEME_OPTIONS } from 'shared/services/themeOptions';
 import { ThemeIcon, ToggleButton } from './ThemeToggler.styled';
+import { setTheme } from 'redux/theme/themeSlice';
 
 export const ThemeToggler = () => {
-  const dispatch = useDispatch();
-  const handleThemeChange = () => {
-    dispatch(toggleTheme());
-  };
   const mode = useSelector(selectTheme);
+  const dispatch = useDispatch();
+
+  const handleThemeChange = () => {
+    dispatch(
+      setTheme(
+        mode === THEME_OPTIONS.light ? THEME_OPTIONS.dark : THEME_OPTIONS.light
+      )
+    );
+  };
 
   return (
     <div>
--- src/shared/styles/CustomThemeProvider.jsx
+++ src/shared/styles/CustomThemeProvider.jsx
@@ -1,29 +1,32 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { selectIsThemeRefreshing, selectTheme } from 'redux/theme/selectors';
-import { refreshTheme } from 'redux/theme/operations';
+import { useEffect, useState } from 'react';
+import { selectTheme } from 'redux/theme/selectors';
 import { ThemeProvider } from '@emotion/react';
 import { THEME_OPTIONS } from 'shared/services/themeOptions';
 import { Global } from '@emotion/react';
 import { GlobalStyles } from 'shared/styles/GlobalStyles.styled';
 
 import { themeLight, themeDark } from './theme';
+import { useAuth } from 'hooks/useAuth';
+import { setTheme } from 'redux/theme/themeSlice';
 
 export const CustomThemeProvider = ({ children }) => {
-  const isRefreshing = useSelector(selectIsThemeRefreshing);
+  const [isThemeRefreshing, setIsThemeRefreshing] = useState(true);
+  const { accessToken } = useAuth();
   const dispatch = useDispatch();
   const mode = useSelector(selectTheme);
 
   useEffect(() => {
-    dispatch(refreshTheme());
-  }, [dispatch]);
+    if (!accessToken) dispatch(setTheme(THEME_OPTIONS.light));
+    setIsThemeRefreshing(false);
+  }, [dispatch, accessToken]);
 
   return (
     <ThemeProvider
       theme={mode === THEME_OPTIONS.light ? themeLight : themeDark}
     >
       <Global styles={GlobalStyles} />
-      {isRefreshing ? null : children}
+      {isThemeRefreshing ? null : children}
     </ThemeProvider>
   );
 };
